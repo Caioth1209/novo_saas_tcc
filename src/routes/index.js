80bc9ec21ab5daf1b9c955f50e4bbee6b3db4465
Caio Araujo <caiodaluz2004@gmail.com>
2024-08-19 17:44:12 -0300

    update

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,5 +4,8 @@ import generateController from "../controllers/generateTccController.js";
 const generateRoute = Router()
 
 generateRoute.post('/tcc', generateController.generateAsyncTcc)
+generateRoute.get('/teste', (req,res)=> {
+    res.send({message: true})
+})
 
 export default generateRoute
