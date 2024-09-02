6ebcde04643554528fca81066ad436ea57ad56ae
IvanAires23 <ivanaires32@gmail.com>
2024-09-02 19:02:45 -0300

    fix: unico thread

--- src/controllers/generateTccController.js
+++ src/controllers/generateTccController.js
@@ -137,7 +137,7 @@ async function generateAsyncTcc(req, res) {
         const { tema, areaEstudo, objetivo, perguntaPesquisa, gerouTcc } = docSnapshot.data()
 
         if (!tema || !areaEstudo || !objetivo || !perguntaPesquisa) {
-            return res.status(400).send("Dados invalidossssssssssss")
+            return res.status(400).send("Dados invalidos")
         } else if (gerouTcc) {
             return res.status(400).send("Este Trabalho Ja foi gerado")
         }
